91ad371ce2db26efcf7fcc27dfb20545d63758f1
Tiffani <tiffani.grace.elmore@gmail.com>
2019-06-27 10:55:19 -0500

    send default response

--- src/main/java/github/grace000/erversay/ServerThread.java
+++ src/main/java/github/grace000/erversay/ServerThread.java
@@ -5,28 +5,44 @@ import java.net.*;
 
 public class ServerThread extends Thread {
     private Socket socket;
+    private InputStream inputStream;
 
     public ServerThread(Socket socket) {
         this.socket = socket;
     }
 
     public void run() {
+        socketGetInputStream();
+        BufferedReader input = new BufferedReader(new InputStreamReader(inputStream));
+        String response = new Response().build();
+        writeResponse(socket, response);
+
+        System.out.println("Message sent");
         try {
-
-            OutputStream outputFromServer = socket.getOutputStream();
-            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
-
-            String response = new Response().build();
-            serverPrintOut.println(response);
-            serverPrintOut.flush();
-
-            System.out.println("Message sent");
-
+            socket.close();
         } catch (IOException e) {
             System.out.println("Server exception: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
+    public void writeResponse(Socket socket, String response) {
+        try {
+            OutputStream outputFromServer = socket.getOutputStream();
+            PrintWriter output = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
+            output.println(response);
+            output.flush();
+        } catch (IOException e) {
+            System.out.println("WRITE RESPONSE ERROR");
+            e.printStackTrace();
+        }
+    }
 
+    private void socketGetInputStream() {
+        try {
+            inputStream = socket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
