2b84be09d628330809dfbf9e31a66b3ac5747a2b
Tiffani <tiffani.grace.elmore@gmail.com>
2019-07-19 13:35:35 -0500

    content header typo

--- src/main/java/github/grace000/erversay/Constants/Headers.java
+++ src/main/java/github/grace000/erversay/Constants/Headers.java
@@ -3,7 +3,7 @@ package github.grace000.erversay.Constants;
 public final class Headers {
     public final static String OPTIONS_HEADER = "Allow: OPTIONS, GET, HEAD";
     public final static String OPTIONS_TWO_HEADER = "Allow: OPTIONS, GET, HEAD, PUT, POST";
-    public final static String CONTENT_LENGTH = "content-length";
+    public final static String CONTENT_LENGTH = "Content-Length";
     public final static String NOT_ALLOWED_HEADER = "Allow: HEAD, OPTIONS";
-    public final static String REDIRECT_HEADER = "Location: http://127.0.0.1:5000/simple_get";
+    public final static String REDIRECT_HEADER = "Location: https://erversay.herokuapp.com/simple_get";
 }
