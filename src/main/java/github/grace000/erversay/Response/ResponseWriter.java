255f018635c3dc80ee60cafa0ad2f089eada6eb2
Tiffani <tiffani.grace.elmore@gmail.com>
2019-07-24 10:16:03 -0500

    delete printout statements from writer

--- src/main/java/github/grace000/erversay/Response/ResponseWriter.java
+++ src/main/java/github/grace000/erversay/Response/ResponseWriter.java
@@ -7,24 +7,11 @@ import static github.grace000.erversay.Constants.HTTPLines.*;
 public class ResponseWriter {
     public void write(OutputStream outputStream, Response response) {
         try {
-            PrintWriter output = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"), true);
-            System.out.println("Here is your header: " + response.headers);
-//            System.out.println("Here is your body: " + response.body.toString());
-            System.out.println("Here is your status!: " + response.status);
-
-
-//            output.println(DEFAULT_VERSION + SP + response.status);
-//            output.println(response.headers);
-//            output.println("Content-Length: " + response.contentLength);
             byte[] status = (DEFAULT_VERSION + SP + response.status + CRLF).getBytes();
             byte[] headers = (response.headers).getBytes();
-//            byte[] content = ("Content-Length: " + response.contentLength + CRLF).getBytes();
 
             outputStream.write(status);
             outputStream.write(headers);
-//            outputStream.write(content);
-//            outputStream.write(response.body);
-//            output.print(response.body);
             outputStream.write((DOUBLE_LINE_FEED).getBytes());
             outputStream.write(response.body);
 
